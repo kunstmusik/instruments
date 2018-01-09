5a0faa2aea01884fda26a17c1e454006b2da5500
Steven Yi <stevenyi@gmail.com>
2018-01-09 18:53:02 -0500

    Revert "use abs of identifier to ensure it works with note format"
    This reverts commit dcc20de902188819776b3c932fc1d513d80b623f.

--- mt0/mt0.js
+++ mt0/mt0.js
@@ -59,7 +59,7 @@ function touchStart(evt) {
 
   for (var i = 0; i < changedTouches.length; i++) {
     let t = changedTouches[i];
-    let id = Math.abs(t.identifier);
+    let id = t.identifier;
     touches[id] = {x: t.clientX, y: t.clientY};
     cs.readScore("i1." + id + " 0 -1 " + id); 
     cs.setControlChannel("touch." + id + ".x", 
@@ -75,9 +75,8 @@ function touchEnd(evt) {
   var changedTouches = evt.changedTouches;
   for (var i = 0; i < changedTouches.length; i++) {
     let t = changedTouches[i];
-    let id = Math.abs(t.identifier);
-    cs.readScore("i-1." + id +" 0 1");
-    delete touches[id];
+    cs.readScore("i-1." + t.identifier +" 0 1");
+    delete touches[t.identifier];
   }
   drawTouches();
 }
@@ -87,8 +86,7 @@ function touchMove(evt) {
   var changedTouches = evt.changedTouches;
   for (var i = 0; i < changedTouches.length; i++) {
 		let t = changedTouches[i];
-    let id = Math.abs(t.identifier);
-    let pt = touches[id];
+    let pt = touches[t.identifier];
     pt.x = t.clientX;
     pt.y = t.clientY;
   }
